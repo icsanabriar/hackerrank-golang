3d7cc3e916035f05e255f4716bc238202a7a372b
Iván Camilo Sanabria <icsanabriar@googlemail.com>
2022-09-01 19:52:55 +0200

    FUN-9: Add math practices.

---
+++ math/medium/gcd/gcd.go
@@ -0,0 +1,46 @@
+// Copyright 2022 Iván Camilo Sanabria
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package main
+
+// minFactor calculates the given n could be decompose.
+func minFactor(n int32) int32 {
+	for i := int32(2); i*i <= n; i++ {
+		if n%i == 0 {
+			return i
+		}
+	}
+	return n
+}
+
+// gcd calculate the great common divisor of given a,b.
+func gcd(a int32, b int32) int32 {
+	if b == 0 {
+		return a
+	} else {
+		return gcd(b, a%b)
+	}
+}
+
+// easyGcd Finds the maximum number that could be part of the arr given the k boundary.
+func easyGcd(k int32, arr []int32) int32 {
+	g := arr[0]
+
+	for i := 1; i < len(arr); i++ {
+		g = gcd(g, arr[i])
+	}
+
+	min := minFactor(g)
+
+	return k / min * min
+}
---
+++ math/medium/gcd/gcd_test.go
@@ -0,0 +1,60 @@
+// Copyright 2022 Iván Camilo Sanabria
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package main
+
+import (
+	"testing"
+)
+
+// TestEasyGcdFirstGivenCase implements the test given as first example on hackerrank.
+func TestEasyGcdFirstGivenCase(t *testing.T) {
+
+	k := int32(5)
+	arr := []int32{2, 6, 4}
+	expected := int32(4)
+
+	result := easyGcd(k, arr)
+
+	if result != expected {
+		t.Errorf("Easy gcd first case was incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+// TestEasyGcdSecondGivenCase implements the test given as second example on hackerrank.
+func TestEasyGcdSecondGivenCase(t *testing.T) {
+
+	k := int32(5)
+	arr := []int32{7}
+	expected := int32(0)
+
+	result := easyGcd(k, arr)
+
+	if result != expected {
+		t.Errorf("Easy gcd second case was incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+// TestEasyGcdHiddenCase implements the test given as hidden example on hackerrank.
+func TestEasyGcdHiddenCase(t *testing.T) {
+
+	k := int32(999999999)
+	arr := []int32{1000000000, 1000000000}
+	expected := int32(999999998)
+
+	result := easyGcd(k, arr)
+
+	if result != expected {
+		t.Errorf("Easy gcd hidden case was incorrect, got: %d, want: %d.", result, expected)
+	}
+}
