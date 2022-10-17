817137bd70376d310b3b65c8cef7b3c23d7c0727
Iván Camilo Sanabria <icsanabriar@googlemail.com>
2022-10-17 23:58:53 +0200

    FUN-10: Add sort practices.

---
+++ sort/easy/insertion/insertion.go
@@ -0,0 +1,42 @@
+// Copyright 2022 Iván Camilo Sanabria
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+// insertionSort return the array used to store the values for counting sort algorithm.
+func insertionSort(w io.Writer, n int32, arr []int32) {
+	temp := arr[n-1]
+	index := n - 2
+
+	for index >= 0 && arr[index] > temp {
+		arr[index+1] = arr[index]
+		printArray(w, arr)
+		index--
+	}
+
+	arr[index+1] = temp
+	printArray(w, arr)
+}
+
+// printArray prints the given array into the console.
+func printArray(w io.Writer, arr []int32) {
+	for _, v := range arr {
+		_, _ = fmt.Fprintf(w, "%d ", v)
+	}
+	_, _ = fmt.Fprintln(w, "")
+}
